31f2b0a7cbc2a8d4d82ce7767ae18e64ff2cec47
optimus2399 <43646673+optimus2399@users.noreply.github.com>
2020-09-12 18:31:12 +0530

    Movement anim

--- BountyPunk-TEST/Assets/Scripts/Player.cs
+++ BountyPunk-TEST/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 3f;
     CharacterController controller;
-    [SerializeField] Animator anim;    //Uday Don
+    [SerializeField] Animator anim;    //Uday noob
 
     // Start is called before the first frame update
     void Start()
@@ -15,41 +15,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ChareterControllerMove();
-        //Move();
-        
-        
+        Move();
     }
 
-    private void ChareterControllerMove()
+    void Move()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-       
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if (direction.magnitude >= 0.1f)
+        var deltaX = horizontal * Time.deltaTime*moveSpeed;
+        var deltaZ = vertical * Time.deltaTime*moveSpeed;
+
+        var newXPos = transform.position.x + deltaX;
+        var newZPos = transform.position.z + deltaZ;
+        var newYPos = transform.position.y;
+
+        var direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        
+        if(direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * moveSpeed * Time.deltaTime);
+            transform.position = new Vector3(newXPos, newYPos, newZPos);
             anim.SetBool("Running", true);
         }
         else
         {
             anim.SetBool("Running", false);
         }
-        
-    }
-
-    void Move()
-    {
-        var deltaX = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
-        var deltaZ = Input.GetAxis("Vertical")*Time.deltaTime*moveSpeed;
-
-        var newXPos = transform.position.x + deltaX;
-        var newZPos = transform.position.z + deltaZ;
-
-        transform.position = new Vector3(newXPos, 0, newZPos);
-        anim.SetFloat("Running", deltaX);
         
     }
     //gg what is up what is up
