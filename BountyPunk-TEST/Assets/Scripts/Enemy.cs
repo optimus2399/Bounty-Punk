db94bda70ff65f08d54d7957d4039b2ae624fd3e
Udayashankar <48129079+udayashankar1@users.noreply.github.com>
2020-09-18 19:27:55 +0530

    Enemy Animations

---
+++ BountyPunk-TEST/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            anim.SetBool("EnemyShoot", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            anim.SetBool("EnemyShoot", false);
+        }
+    }
+}
