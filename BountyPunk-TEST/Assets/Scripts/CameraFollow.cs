d14257565106a477f4083226327c91b8f17e8f3c
Udayashankar <48129079+udayashankar1@users.noreply.github.com>
2020-10-17 18:19:19 +0530

    CAMERA

---
+++ BountyPunk-TEST/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    public float smoothSpeed = 0.1f;
+    public Vector3 offset;
+
+
+    private void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothPosition;
+
+        transform.LookAt(target);
+
+    }
+}
