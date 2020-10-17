c015db31dd84221cc10c4289c9276b1fa0026a80
optimus2399 <43646673+optimus2399@users.noreply.github.com>
2020-10-17 13:42:40 +0530

    AI

---
+++ BountyPunk-TEST/Assets/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public NavMeshAgent agent;
+    Animator anim;
+    public GameObject pistol;
+    public GameObject bullet;
+    public GameObject firePoint;
+
+
+    public GameObject GetPlayer() { return player; }
+    public NavMeshAgent GetAgent() { return agent; }
+    public GameObject GetPistol() { return pistol; }
+    public GameObject GetBullet() { return bullet; }
+    public GameObject GetFirePoint() { return firePoint; }
+
+   public void Fire()
+    {
+        GameObject projectile = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+    }
+
+    public void StartFiring()
+    {
+        InvokeRepeating("Fire", 0.5f, 0.5f);
+    }
+    public void StopFiring()
+    {
+        CancelInvoke("Fire");
+    }
+    // Start is called before the first frame update
+   void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Distance", Vector3.Distance(transform.position, player.transform.position));
+    }
+}
---
+++ BountyPunk-TEST/Assets/EnemyBaseSM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBaseSM : StateMachineBehaviour
+{
+    public GameObject enemy;
+    public GameObject player;
+    public NavMeshAgent agent;
+    public GameObject pistol;
+    public GameObject bullet;
+    public GameObject firePoint;
+
+    // Start is called before the first frame update
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy = animator.gameObject;
+        player = enemy.GetComponent<EnemyAI>().GetPlayer();
+        agent = enemy.GetComponent<EnemyAI>().GetAgent();
+        pistol = enemy.GetComponent<EnemyAI>().GetPistol();
+        bullet = enemy.GetComponent<EnemyAI>().GetBullet();
+        firePoint = enemy.GetComponent<EnemyAI>().GetFirePoint();
+    }
+
+   
+}
