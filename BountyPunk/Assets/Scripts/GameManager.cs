f785879d22c386f6b26366d2c252c999fad42fd1
udayashankar1 <48129079+udayashankar1@users.noreply.github.com>
2021-06-14 12:38:59 +0530

    lighting

--- BountyPunk/Assets/Scripts/GameManager.cs
+++ BountyPunk/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int enemyDeathCount=0;
+     int enemyDeathCount=0;
     [SerializeField] GameObject[] barriers;
     [SerializeField] int[] totalEnemyCount;
 
